7d630f80c1d4183da8029d2126983455a5c1204e
djwood95 <djwood@mtu.edu>
2020-12-28 12:00:10 -0500

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 npm run build
-scp -r ./dist djwood@DO-kmtracker2:/var/www/html/kmtracker
-scp -r ./api/classes djwood@DO-kmtracker2:/var/www/html/kmtracker/api
-scp -r ./api/public djwood@DO-kmtracker2:/var/www/html/kmtracker/api
-scp -r ./api/src djwood@DO-kmtracker2:/var/www/html/kmtracker/api
-scp ./api/composer.json djwood@DO-kmtracker2:/var/www/html/kmtracker/api
-ssh djwood@DO-kmtracker2 'cd /var/www/html/kmtracker/api; composer install'
+scp -r ./dist djwood@kmtracker:/var/www/html/kmtracker
+scp -r ./api/classes djwood@kmtracker:/var/www/html/kmtracker/api
+scp -r ./api/public djwood@kmtracker:/var/www/html/kmtracker/api
+scp -r ./api/src djwood@kmtracker:/var/www/html/kmtracker/api
+scp ./api/composer.json djwood@kmtracker:/var/www/html/kmtracker/api
+ssh djwood@kmtracker 'cd /var/www/html/kmtracker/api; composer install'
